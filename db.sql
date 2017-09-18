e14e99e8fce33e9bfdaf0aa30d51d693c86b9a87
md98 <makerdark98@naver.com>
2017-09-18 10:57:20 +0900

    init

--- db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS Posts (
-    idx INTEGER PRIMARY KEY,
-    title TEXT,
-    recent_date DATE,
-    readCheck INTEGER,
-    Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-);
